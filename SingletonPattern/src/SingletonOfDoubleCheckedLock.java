56859178034a74f971f3bc6fae9702f8633c49bf
BianWenKai <1467295546@qq.com>
2021-10-12 13:59:31 +0800

    双重检查加锁：添加一些注释

--- SingletonPattern/src/SingletonOfDoubleCheckedLock.java
+++ SingletonPattern/src/SingletonOfDoubleCheckedLock.java
@@ -4,10 +4,12 @@
  **/
 public class SingletonOfDoubleCheckedLock {
 
+    //通过volatile关键字，确保多线程正确的处理 uniqueInstance 变量
     private volatile static SingletonOfDoubleCheckedLock uniqueInstance;
 
     private SingletonOfDoubleCheckedLock(){}
 
+    //检查实例是否创建，如果尚未创建，则进行同步。这样只有第一次会同步。
     public SingletonOfDoubleCheckedLock getInstance(){
         if (uniqueInstance == null) {
             synchronized (SingletonOfDoubleCheckedLock.class) {
