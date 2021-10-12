4b718e9ab9fa8ff9a802d4c5825d3b76dd24c028
BianWenKai <1467295546@qq.com>
2021-10-12 13:03:23 +0800

    单例模式——饿汉式

--- SingletonPattern/src/SingletonOfEagerly.java
+++ SingletonPattern/src/SingletonOfEagerly.java
@@ -1,18 +1,16 @@
 /**
  * @author BianWenKai
- * @DATE 2021/10/12 - 9:54
+ * @DATE 2021/10/12 - 10:45
  **/
-public class Singleton {
+public class SingletonOfEagerly {
 
-    //懒汉式
-    private static Singleton uniqueInstance = null;
+    //饿汉式
+    private static SingletonOfEagerly uniqueInstance = new SingletonOfEagerly();
 
-    private Singleton(){}
+    private SingletonOfEagerly(){}
     //多线程下会产生多个实例变量，所以通过增加synchronized关键字在实例化方法中，迫使每个线程在进入该方法之前，必须等别的线程离开该方法，即不能有多个线程同时进入该方法。
-    public static synchronized Singleton getInstance(){
-        if (uniqueInstance == null){
-            uniqueInstance = new Singleton();
-        }
+    public static SingletonOfEagerly getInstance(){
         return uniqueInstance;
     }
+
 }
