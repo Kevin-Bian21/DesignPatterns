53890df7615e7365079d86ca14196418162013ea
BianWenKai <1467295546@qq.com>
2021-12-06 13:46:08 +0800

    修改gitignore文件

--- src/strategy/fly/FlyBehavior.java
+++
@@ -1,9 +0,0 @@
-package strategy.fly;
-
-/**
- * @author BianWenKai
- * @Data 2021/6/2 - 19:39
- **/
-public interface FlyBehavior {
-    void fly();
-}
--- src/strategy/quack/QuackBehavior.java
+++
@@ -1,9 +0,0 @@
-package strategy.quack;
-
-/**
- * @author BianWenKai
- * @Data 2021/6/2 - 20:16
- **/
-public interface QuackBehavior {
-    void quack();
-}
